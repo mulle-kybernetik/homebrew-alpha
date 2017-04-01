e997de33165667a00e83f6fed7b6ec58b89798ca
Nat! <nat@mulle-kybernetik.com>
2017-04-01 17:12:05 +0200

    3.1.3 alpha-release of mulle-bootstrap

--- mulle-bootstrap.rb
+++ mulle-bootstrap.rb
@@ -1,16 +1,9 @@
 class MulleBootstrap < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap"
   desc "Dependency manager for cross-platform projects"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/3.1.2"
-  version "3.1.2"
-  sha256 "50776d33fb2d4c1f7d8441cc5c3ca2d6852b116dd67056e9c4e93d31ed49b4a9"
-
-  depends_on :git  => :run
-  depends_on :curl => :run
-  depends_on :bash => :run
-
-# we can live w/o cmake though
-  depends_on :cmake => :run
+  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/3.1.3"
+  version "3.1.3"
+  sha256 "e78ad05bb23e6653b46190fbfc45affdc36637c4c9d4dd7f82914fc2a432cefd"
 
   def install
      system "./install.sh", "#{prefix}"
