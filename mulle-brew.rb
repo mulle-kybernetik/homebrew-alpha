bacbfdfdfe77ebe122b8e1c2516908617f0f8dda
Nat! <nat@mulle-kybernetik.com>
2017-04-24 18:07:21 +0200

    3.5.2 alpha-release of mulle-brew

--- mulle-brew.rb
+++ mulle-brew.rb
@@ -1,9 +1,9 @@
 class MulleBrew < Formula
   homepage "https://www.mulle-kybernetik.com/software/git/mulle-brew"
   desc "Dependency manager for homebrew depedencies"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-brew/tarball/3.5.1"
-  version "3.5.1"
-  sha256 "9481731bb5e7d2767007c794a74ba6a1b4b130b54226aa2d4ddcfcf4369161e6"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-brew/tarball/3.5.2"
+  version "3.5.2"
+  sha256 "ce1f8f6bd424d1a54c343b54c9b67ed11435054ec4d63086eb22ad8cbe7e32d8"
 
   depends_on "mulle-kybernetik/alpha/mulle-bootstrap"
 
