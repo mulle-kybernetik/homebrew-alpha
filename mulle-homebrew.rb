9a1a262b148c08dce4ce7e6b19177bfcb9eda697
Nat! <nat@mulle-kybernetik.com>
2017-05-03 22:52:43 +0200

    1.0.3 release of mulle-homebrew

--- mulle-homebrew.rb
+++ mulle-homebrew.rb
@@ -1,15 +1,16 @@
 class MulleHomebrew < Formula
    homepage "https://github.com/mulle-nat/mulle-homebrew"
    desc "Release and publish a project to a homebrew tap"
-   url "https://github.com/mulle-nat/mulle-homebrew/archive/1.0.2.tar.gz"
-   version "1.0.2"
-   sha256 "a3877cc97ceadc6c097350815b3b3ba54433d505bfe79d7438406242a305314e"
+   url "https://github.com/mulle-nat/mulle-homebrew/archive/1.0.3.tar.gz"
+   version "1.0.3"
+   sha256 "0f16b247b63514791ee3213f007ffd54c10fda5b74a30f6b180ac36454a61c07"
 
    depends_on 'mulle-kybernetik/alpha/mulle-bootstrap' => :build
    depends_on 'mulle-kybernetik/alpha/mulle-build' => :build
+   depends_on 'cmake' => :build
 
    def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
+      system "mulle-install", "-vvv", "--prefix", "#{prefix}", "--homebrew"
    end
 
    test do
