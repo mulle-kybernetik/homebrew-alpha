f348bc178b6e86c3f3b9f5bdd655a4b1fe8948ea
Nat! <nat@codeon.de>
2017-05-05 19:13:14 +0200

    3.0.2 release of mulle-homebrew

--- mulle-homebrew.rb
+++ mulle-homebrew.rb
@@ -1,9 +1,9 @@
 class MulleHomebrew < Formula
    homepage "https://github.com/mulle-nat/mulle-homebrew"
    desc "Release and publish a project to a homebrew tap"
-   url "https://github.com/mulle-nat/mulle-homebrew/archive/3.0.1.tar.gz"
-   version "3.0.1"
-   sha256 "ff3ce129b41de334b8a4230e33287bb8c9e258fc940ad65cb5fc6927b6b557c8"
+   url "https://github.com/mulle-nat/mulle-homebrew/archive/3.0.2.tar.gz"
+   version "3.0.2"
+   sha256 "e2b86eb75b38aa21d93a5341324dd49e0a4be3768432465aa0dda5adc4a08b5e"
 
    depends_on 'mulle-kybernetik/alpha/mulle-bootstrap' => :build
    depends_on 'mulle-kybernetik/alpha/mulle-build' => :build
