70a0c0d894674201a5092d1fb0a9f99d9f6dc5f8
Nat! <nat@mulle-kybernetik.com>
2017-05-06 13:54:09 +0200

    3.3.0 release of mulle-homebrew

--- mulle-homebrew.rb
+++ mulle-homebrew.rb
@@ -1,20 +1,22 @@
 class MulleHomebrew < Formula
-   homepage "https://github.com/mulle-nat/mulle-homebrew"
-   desc "Release and publish a project to a homebrew tap"
-   url "https://github.com/mulle-nat/mulle-homebrew/archive/3.1.0.tar.gz"
-   version "3.1.0"
-   sha256 "834fab56271ae4e1543e379f4231dbe93996f10797b29c2b579853e1c184bfd5"
+  desc "Release and publish a project to a homebrew tap"
+  homepage "https://github.com/mulle-nat/mulle-homebrew"
+  url "https://github.com/mulle-nat/mulle-homebrew/archive/3.3.0.tar.gz"
+  sha256 "009b0fabda932139f8d18f8eeb5c67c4b37e56274b716e2abe4ecd11c4cfce86"
+  # version "3.3.0"
 
-   depends_on 'mulle-kybernetik/alpha/mulle-bootstrap' => :build
-   depends_on 'mulle-kybernetik/alpha/mulle-build' => :build
-   depends_on 'cmake' => :build
+  depends_on "mulle-kybernetik/alpha/mulle-bootstrap" => :build
+  depends_on "mulle-kybernetik/alpha/mulle-build" => :build
+  depends_on "cmake" => :build
 
-   def install
-      system "mulle-install", "-vvv", "--prefix", "#{prefix}", "--homebrew" 
-   end
+  def install
+    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
+  end
 
-   test do
-      system "mulle-test", "-vvv", "--fast-test" 
-   end
+  test do
+    if File.directory? 'tests'
+      system "mulle-test", "-vvv", "--fast-test"
+    end
+  end
 end
 # FORMULA mulle-homebrew.rb
